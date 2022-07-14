c37e0216340a52db166b5c36f59600a16ec03112
zeroleo12345 <zeroleo12345@163.com>
2022-07-14 22:36:47 +0800

    m

--- sum_u32/src/main.rs
+++ sum_u32/src/main.rs
@@ -1,6 +1,7 @@
 fn sum(numbers: &[u32]) -> Option<u32> {
     let mut total: u32 = 0;
     for num in numbers.iter() {
+        print!(" + {} ", *num);
         match total.checked_add(*num) {
             Some(value) => {
                 total = value;
@@ -17,19 +18,19 @@ fn main() {
     let numbers1: [u32; 5] = [1, 2, 3, 4, 5];
     match sum(&numbers1) {
         Some(value) => {
-            println!("sum {value}");
+            println!("= {value}");
         }
         None => {
-            println!("overflow");
+            println!("is overflow");
         }
     }
     let numbers2: [u32; 5] = [1, 2, 3, 4, 4294967294];
     match sum(&numbers2) {
         Some(value) => {
-            println!("sum {value}");
+            println!("= {value}");
         }
         None => {
-            println!("overflow");
+            println!("is overflow");
         }
     }
 }
