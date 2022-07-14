10d288a81f845ee9b34158ae9a2b739bead3c4bf
zeroleo12345 <zeroleo12345@163.com>
2022-07-14 23:46:20 +0800

    enhancement

--- sum_u32/src/main.rs
+++ sum_u32/src/main.rs
@@ -15,7 +15,7 @@ fn sum(numbers: &[u32]) -> Option<u32> {
 }
 
 fn main() {
-    let numbers1: [u32; 5] = [1, 2, 3, 4, 5];
+    let numbers1: Vec<u32> = vec![1, 2, 3, 4, 5];
     match sum(&numbers1) {
         Some(value) => {
             println!("= {value}");
@@ -24,7 +24,7 @@ fn main() {
             println!("is overflow");
         }
     }
-    let numbers2: [u32; 5] = [1, 2, 3, 4, 4294967294];
+    let numbers2: Vec<u32> = vec![1, 2, 3, 4, 4294967294];
     match sum(&numbers2) {
         Some(value) => {
             println!("= {value}");
