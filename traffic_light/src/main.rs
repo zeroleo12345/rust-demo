bb2339896535bfc09c5be43651e73fa9bc0e98a6
zeroleo12345 <zeroleo12345@163.com>
2022-07-14 21:31:41 +0800

    finish traffic_light

--- traffic_light/src/main.rs
+++ traffic_light/src/main.rs
@@ -1,3 +1,29 @@
+pub enum TrafficLight {
+    Red,
+    Green,
+    Yellow,
+}
+
+trait Light {
+    // seconds
+    fn duration(self) -> u32;
+}
+
+impl Light for TrafficLight {
+    fn duration(self) -> u32 {
+        match self {
+          TrafficLight::Red => 3,
+          TrafficLight::Green => 5,
+          TrafficLight::Yellow => 7,
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let red_time = TrafficLight::Red.duration();
+    let green_time = TrafficLight::Green.duration();
+    let yellow_time = TrafficLight::Yellow.duration();
+    println!("Red Light time: {red_time}");
+    println!("Green Light time: {green_time}");
+    println!("Yellow Light time: {yellow_time}");
 }
